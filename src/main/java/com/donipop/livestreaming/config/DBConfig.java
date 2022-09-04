d7af782487202160d72bc39f6174335484d14a49
doni pop <minjaebae@gmail.com>
2022-09-04 21:56:28 +0900

    index.html 유저화면 작업완료

---
+++ src/main/java/com/donipop/livestreaming/config/DBConfig.java
@@ -0,0 +1,33 @@
+package com.donipop.livestreaming.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.sql.DataSource;
+@Slf4j
+@Configuration
+@PropertySource("classpath:/application.yml")
+public class DBConfig {
+
+    @Autowired
+    ApplicationContext applicationContext;
+    @Bean
+    @ConfigurationProperties(prefix="spring.datasource.hikari")
+    public HikariConfig hikariConfig(){
+        return new HikariConfig();
+    }
+
+    @Bean
+    public DataSource dataSource() throws Exception{
+        DataSource dataSource = new HikariDataSource(hikariConfig());
+        log.info(dataSource.toString());
+        return dataSource;
+    }
+}
---
+++ src/main/java/com/donipop/livestreaming/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.donipop.livestreaming.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class IndexController {
+
+    @GetMapping("index")
+    public String getMain(){
+
+        return "/index";
+    }
+}
