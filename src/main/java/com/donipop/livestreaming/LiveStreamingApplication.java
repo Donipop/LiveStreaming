f959a6ea744cf582be137fba99b4956efe5a6caf
doni pop <minjaebae@gmail.com>
2022-09-07 02:08:37 +0900

    css복구

--- src/main/java/com/donipop/livestreaming/LiveStreamingApplication.java
+++ src/main/java/com/donipop/livestreaming/LiveStreamingApplication.java
@@ -1,9 +1,11 @@
 package com.donipop.livestreaming;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.donipop.livestreaming")
 public class LiveStreamingApplication {
 
     public static void main(String[] args) {
