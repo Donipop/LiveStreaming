bc701131b13696757663b2950ff34b8a99f326c1
doni pop <minjaebae@gmail.com>
2022-09-27 00:39:21 +0900

    view페이지 수정중

--- src/main/java/com/donipop/livestreaming/controller/ChatController.java
+++ src/main/java/com/donipop/livestreaming/controller/ChatController.java
@@ -4,9 +4,6 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 //@RestController
 @Controller
--- src/main/java/com/donipop/livestreaming/controller/ViewController.java
+++ src/main/java/com/donipop/livestreaming/controller/ViewController.java
@@ -3,12 +3,19 @@ package com.donipop.livestreaming.controller;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Log4j2
 @Controller
 public class ViewController {
     @GetMapping("view")
     public String getView(){
+        return "/view";
+    }
+
+    @GetMapping("view/{channel}")
+    public String get_channel_view(@PathVariable String channel){
+
         return "/view";
     }
 }
